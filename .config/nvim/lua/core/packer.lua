4e7a79fb8e6558df3171fa0b60956110e53fc80f
Matthias Kuklinski <contact@matthias-kuklinski.com>
2022-01-18 21:04:11 +0100

    Add necessary plugins for auto-completion

--- .config/nvim/lua/core/packer.lua
+++ .config/nvim/lua/core/packer.lua
@@ -2,4 +2,8 @@ return require("packer").startup(function()
   use "wbthomason/packer.nvim"
   use "kyazdani42/nvim-web-devicons"
   use "kyazdani42/nvim-tree.lua"
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-buffer"
+  use "L3MON4D3/LuaSnip"
+  use "onsails/lspkind-nvim"
 end)
