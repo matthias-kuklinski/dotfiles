452e74d5288562f2823326227e55d14890d2ea14
Matthias Kuklinski <contact@matthias-kuklinski.com>
2022-05-29 18:40:47 +0200

    Map keys for buffer/window splitting

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -7,4 +7,7 @@ map('n', '<c-h>', ':wincmd h<CR>', options)
 map('n', '<c-j>', ':wincmd j<CR>', options)
 map('n', '<c-k>', ':wincmd k<CR>', options)
 map('n', '<c-l>', ':wincmd l<CR>', options)
+map('n', '<c-,>', ':wincmd s<CR>', options)
+map('n', '<c-.>', ':wincmd v<CR>', options)
+
 map('n', '<leader>e', ':NvimTreeToggle<CR>', options)
