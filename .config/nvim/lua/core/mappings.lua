23acdc7febc4023d51984e49ddb3fae245cbc187
Matthias Kuklinski <contact@matthias-kuklinski.com>
2022-05-29 18:39:51 +0200

    Map keys for embedded split navigation

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -3,4 +3,8 @@ local options = { noremap = true, silent = true }
 
 vim.g.mapleader = ' ' -- Space
 
+map('n', '<c-h>', ':wincmd h<CR>', options)
+map('n', '<c-j>', ':wincmd j<CR>', options)
+map('n', '<c-k>', ':wincmd k<CR>', options)
+map('n', '<c-l>', ':wincmd l<CR>', options)
 map('n', '<leader>e', ':NvimTreeToggle<CR>', options)
