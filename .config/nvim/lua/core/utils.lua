3d9cc75c9faa31b2675af469ec7745e6d269dc26
Matthias Kuklinski <contact@matthias-kuklinski.com>
2023-01-01 20:37:01 +0200

    Set mappings

--- .config/nvim/lua/core/utils.lua
+++ .config/nvim/lua/core/utils.lua
@@ -4,6 +4,12 @@ return {
       vim.opt[option] = value
     end
   end,
+
+  set_mappings = function(mode_mappings)
+    for mode, mappings in pairs(mode_mappings) do
+      for keybind, mapping in pairs(mappings) do
+       vim.keymap.set(mode, keybind, mapping, {})
+      end
     end
   end
 }
