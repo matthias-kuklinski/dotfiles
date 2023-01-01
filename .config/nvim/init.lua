4a81259bd900b40e038a153567be74b24681c7c7
Matthias Kuklinski <contact@matthias-kuklinski.com>
2023-01-01 20:48:04 +0200

    Set mappings for multiple categories

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,14 @@
 utils = require("core.utils")
 utils.setOptions(require("core.options"))
+utils.set_mappings(require("core.mappings").general)
+utils.set_mappings(require("core.mappings").movement)
+utils.set_mappings(require("core.mappings").layout)
 
 require("core.packer")
 require("core.mappings")
 require("plugin.nvim-tree")
 require("plugin.nvim-cmp")
+
+require('github-theme').setup({
+  theme_style = "dark_default"
+})
