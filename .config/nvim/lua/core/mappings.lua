7b447b8ba957bbbd2c38caba429ae6b7085029e6
Matthias Kuklinski <contact@matthias-kuklinski.com>
2023-01-14 20:39:55 +0100

    Configure Telescope bindings

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -11,7 +11,10 @@ return {
       ["<c-h>"] = ":wincmd h <CR>",
       ["<c-j>"] = ":wincmd j <CR>",
       ["<c-k>"] = ":wincmd k <CR>",
-      ["<c-l>"] = ":wincmd l <CR>"
+      ["<c-l>"] = ":wincmd l <CR>",
+      ["<leader>ff"] = "<cmd> :Telescope find_files <CR>",
+      ["<leader>fg"] = "<cmd> :Telescope live_grep <CR>",
+      ["<leader>fb"] = "<cmd> :Telescope buffers <CR>"
     }
   },
 
